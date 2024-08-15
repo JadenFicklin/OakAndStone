a717ed3719905eba32cfa756bf65a47012b2e2e7
JadenFicklin <jadendficklin@gmail.com>
2024-08-14 19:41:27 -0600

    breakpoints for frequently asked questions section

--- src/components/LandingFaqs.js
+++ src/components/LandingFaqs.js
@@ -8,42 +8,43 @@ export const LandingFaqs = () => {
   console.log(currentFaq);
   return (
     <>
-      <div></div>
-      <div>
-        {landingFaqData.map((item, index) => (
-          <div
-            key={index}
-            onClick={() => {
-              setCurrentFaq(index);
-            }}
-            className="relative flex flex-wrap w-10/12 mx-auto my-10 cursor-pointer text-brown">
+      <div className="grid items-center grid-cols-1 md:grid-cols-2">
+        <div className="md:order-2">
+          {landingFaqData.map((item, index) => (
             <div
-              className={cn(
-                'h-full w-[3px] absolute -left-6',
-                currentFaq === index ? 'bg-brown' : ''
-              )}></div>
-            <div>
-              <h2 className="text-3xl">{item.title}</h2>
-              {item.subTitle && (
-                <h3 className="pt-3 text-xs opacity-50">{item.subTitle}</h3>
-              )}
-              <p className="py-3 text-xs">
-                {item.text}{' '}
-                {currentFaq === index && (
-                  <Link to={item.link} className="ml-1 text-xs text-blue-400">
-                    {item.button}
-                  </Link>
+              key={index}
+              onClick={() => {
+                setCurrentFaq(index);
+              }}
+              className="relative flex flex-wrap w-10/12 mx-auto my-10 cursor-pointer text-brown">
+              <div
+                className={cn(
+                  'h-full w-[3px] absolute -left-6',
+                  currentFaq === index ? 'bg-brown' : ''
+                )}></div>
+              <div>
+                <h2 className="text-3xl">{item.title}</h2>
+                {item.subTitle && (
+                  <h3 className="pt-3 text-xs opacity-50">{item.subTitle}</h3>
                 )}
-              </p>
+                <p className="py-3 text-xs">
+                  {item.text}{' '}
+                  {currentFaq === index && (
+                    <Link to={item.link} className="ml-1 text-xs text-blue-400">
+                      {item.button}
+                    </Link>
+                  )}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <img
+          className="w-11/12 mx-auto md:order-1 bg-no-repeat bg-cover max-h-[400px] max-w-[800px] my-10 object-cover"
+          src={landingFaqData[currentFaq].image}
+          alt={landingFaqData[currentFaq].imageAlt}
+        />
       </div>
-      <img
-        className="w-11/12 mx-auto"
-        src={landingFaqData[currentFaq].image}
-        alt={landingFaqData[currentFaq].imageAlt}
-      />
     </>
   );
 };
