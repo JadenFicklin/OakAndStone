f2fb36bd02b5f1d5e5fc3b4253205d4807b2027e
JadenFicklin <jadendficklin@gmail.com>
2024-08-16 17:30:19 -0600

    gallery home page

--- src/data/Gallery.js
+++ src/data/Gallery.js
@@ -1,31 +1,43 @@
+import customCabinets from 'assets/images/gallery/custom-cabinets-8.jpg';
+import kitchens from 'assets/images/gallery/kitchen-remodel-3.jpg';
+import bathrooms from 'assets/images/gallery/bathroom-remodel-12.jpg';
+import customWoodwork from 'assets/images/gallery/custom-woodwork-3.jpg';
+import otherProjects from 'assets/images/gallery/other-projects-6.jpg';
+
 export const galleryData = [
   {
     title: 'Custom Cabinets',
     text: 'We are custom cabinet designers and builders, specializing in mid to upper tier cabinets. We use only the best hardware with soft close hinges and soft close under mount drawer slides.',
     button: 'View custom cabinets image gallery',
-    link: 'gallery/custom-cabinets'
+    link: 'gallery/custom-cabinets',
+    image: customCabinets
   },
   {
     title: 'Kitchens',
     text: 'A kitchen remodel is a great way to give your home a new feel, as well as upgrade the value of your home. We can do a wide variety of cabinet, island, drawer, and custom configurations for your home.',
     button: 'View kitchen image gallery',
-    link: 'gallery/kitchen-remodels'
+    link: 'gallery/kitchen-remodels',
+    image: kitchens
   },
   {
     title: 'Bathrooms',
     text: 'We consider ourselves top tier bathroom remodel designer builders. Steam showers, heated floors, Euro glass doors, and more.',
-    link: 'gallery/bathroom-remodels'
+    button: 'View bathrooms image gallery',
+    link: 'gallery/bathroom-remodels',
+    image: bathrooms
   },
   {
     title: 'Custom Woodwork',
-    text: 'We can custom make build ins, book cases, mantles, arches, false beams and more see our pictures.',
+    text: 'We can custom make build ins, book cases, mantles, arches, false beams and more.',
     button: 'View custom woodwork image gallery',
-    link: 'gallery/custom-woodwork'
+    link: 'gallery/custom-woodwork',
+    image: customWoodwork
   },
   {
     title: 'Other Projects',
     text: 'We can build pretty much anything you need.',
     button: 'View other projects image gallery',
-    link: 'gallery/other-projects'
+    link: 'gallery/other-projects',
+    image: otherProjects
   }
 ];
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,7 +1,59 @@
+import { useEffect, useState, useRef } from 'react';
+import { galleryData } from 'data/Gallery';
+import { Link } from 'react-router-dom';
+
 export const Gallery = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const containerRefs = useRef([]);
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <div>Gallery</div>
+      <div className="grid text-white gap-y-10">
+        {galleryData.map((item, index) => (
+          <div
+            key={index}
+            className="relative w-full h-full overflow-hidden"
+            ref={(el) => (containerRefs.current[index] = el)}>
+            {/* image */}
+            <div
+              className="content-center w-full bg-center bg-no-repeat bg-cover pointer-events-none h-[600px] transform scale-[140%] md:scale-[130%] lg:scale-[120%]"
+              style={{
+                backgroundImage: `url(${item.image})`,
+                backgroundPositionY: `${
+                  (scrollPosition -
+                    (containerRefs.current[index]?.offsetTop || 0)) *
+                  0.3
+                }px`
+              }}></div>
+            {/* Overlay */}
+            <div
+              style={{ backgroundColor: 'rgba(143, 99, 70, 0.4)' }}
+              className="absolute inset-0 "></div>
+            {/* text */}
+            <div className="absolute inset-0 grid content-center w-11/12 mx-auto sm:w-9/12 gap-y-10">
+              <h3 className="text-4xl md:text-6xl xl:text-8xl">{item.title}</h3>
+              <p className="text-lg xl:text-2xl">{item.text}</p>
+              <Link
+                to={item.link}
+                className="p-5 duration-150 sm:p-6 text-md xl:text-xl bg-zinc-900 w-max hover:bg-zinc-950">
+                {item.button}
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
